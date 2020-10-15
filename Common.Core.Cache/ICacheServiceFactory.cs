5c264f3b742cdd721a06591fa027488b9d4ffa9d
Jay Shi <JayShi@eurofins.com>
2020-10-15 14:33:32 +0800

    Adding Cache

---
+++ Common.Core.Cache/ICacheServiceFactory.cs
@@ -0,0 +1,9 @@
+﻿namespace Common.Core.Cache
+{
+    public interface ICacheServiceFactory
+    {
+        object Get(string Code);
+
+        object Set(string Code, object value);
+    }
+}
---
+++ Common.Core.Cache/MemoryCache/IMemoryCacheProcess.cs
@@ -0,0 +1,9 @@
+﻿namespace Common.Core.Cache.MemoryCache
+{
+    public interface IMemoryCacheProcess
+    {
+        object Get(string Code);
+
+        object Set(string Code, object value);
+    }
+}
