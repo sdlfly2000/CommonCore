6fe7e27a95ed009aadf7342180a7d0936beb339f
Jay Shi <JayShi@eurofins.com>
2020-10-16 15:58:13 +0800

    Adding Sripts for Version and ToNuget

---
+++ Common.Core/Scripts/SetVersion.ps1
@@ -0,0 +1,29 @@
+﻿##############################################################
+# To Upgrade Version by Patch before build and push to Nuget.
+##############################################################
+
+# No Parameter
+Param()
+
+# Variables
+$projectFileName = "Common.Core.csproj"
+$projectFile = [xml](Get-Content -path $projectFileName)
+
+# Update Version in Project File, version format is like "Major.Minor.Patch". This script only upgrades Patch.
+$propertyGroupNodes = $projectFile.Project.PropertyGroup
+$versionParts = $propertyGroupNodes.Version.ToString().Split(".")
+$major = $versionParts[0]
+$minor = $versionParts[1]
+$patch = $versionParts[2]
+$upgradedPatch = [string]([int]$patch + 1)
+
+$propertyGroupNodes.Version = $major + "." + $minor + "." + $upgradedPatch
+
+# Summary
+Write-Host "-Upgrade Version-" -foregroundcolor "magenta"
+Write-Host "Script:			.\Scripts\SetVersion.ps1"   
+Write-Host "Current Version:	"$major"."$minor"."$patch
+Write-Host "Upgraded Version:	"$major"."$minor"."$upgradedPatch
+
+# Save the chagne
+$projectFile.save($projectFileName)
---
+++ Common.Core/Scripts/ToNuget.ps1
@@ -0,0 +1,17 @@
+﻿##############################################################
+# Push to Nuget.
+##############################################################
+
+# No Parameter
+Param()
+
+# Variables
+$projectFileName = "Common.Core.csproj"
+$projectFile = [xml](Get-Content -path $projectFileName)
+$version = $projectFile.Project.PropertyGroup.Version
+
+# CMD to Push Nuget
+$cmdline = "dotnet nuget push .\Common.Core\bin\Debug\IdeaActivator.Common.Core.$version.nupkg -k oy2niutbai55vj2eic7q3jem3xlmvdnenlm7iz2eg3evli --source https://api.nuget.org/v3/index.json"
+Write-Host $cmdline
+#CMD.exe /c $cmdline
+
