302eaf0ab703b62c03800093af53062c654be032
Jay Shi <JayShi@eurofins.com>
2020-10-16 16:08:46 +0800

    Move key to appsetting

--- Common.Core/Scripts/ToNuget.ps1
+++ Common.Core/Scripts/ToNuget.ps1
@@ -6,12 +6,13 @@
 Param()
 
 # Variables
+
+
 $projectFileName = "Common.Core.csproj"
 $projectFile = [xml](Get-Content -path $projectFileName)
 $version = $projectFile.Project.PropertyGroup.Version
 
 # CMD to Push Nuget
-$cmdline = "dotnet nuget push .\Common.Core\bin\Debug\IdeaActivator.Common.Core.$version.nupkg -k oy2niutbai55vj2eic7q3jem3xlmvdnenlm7iz2eg3evli --source https://api.nuget.org/v3/index.json"
 Write-Host $cmdline
 #CMD.exe /c $cmdline
 
