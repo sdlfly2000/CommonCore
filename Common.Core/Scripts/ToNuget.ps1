dd5a42837edc1683826d6c79b44f1393a2ae3bc0
Jay Shi <JayShi@eurofins.com>
2020-10-16 16:45:37 +0800

    Adding appsetting reading

--- Common.Core/Scripts/ToNuget.ps1
+++ Common.Core/Scripts/ToNuget.ps1
@@ -6,13 +6,19 @@
 Param()
 
 # Variables
+$appSettingFileName = "appsetting.json"
+$appSetting = (Get-Content -path $appSettingFileName) | ConvertFrom-Json
+$uploadToNuget = [bool]$appSetting.UploadToNuget
+
+if($uploadToNuget){
+	$projectFileName = "Common.Core.csproj"
+	$projectFile = [xml](Get-Content -path $projectFileName)
+	$version = $projectFile.Project.PropertyGroup.Version
+
+	# CMD to Push Nuget
+	Write-Host $cmdline
+	#CMD.exe /c $cmdline
+}
 
 
-$projectFileName = "Common.Core.csproj"
-$projectFile = [xml](Get-Content -path $projectFileName)
-$version = $projectFile.Project.PropertyGroup.Version
-
-# CMD to Push Nuget
-Write-Host $cmdline
-#CMD.exe /c $cmdline
 
