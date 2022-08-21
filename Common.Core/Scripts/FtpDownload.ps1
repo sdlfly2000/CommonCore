85f5bf41bf5cf4d8371cc5985229d2845054d0e1
Jay Shi <25982097@qq.com>
2022-08-21 11:05:26 +0800

    Update to .Net6 and ftp server Ip

--- Common.Core/Scripts/FtpDownload.ps1
+++ Common.Core/Scripts/FtpDownload.ps1
@@ -3,7 +3,7 @@ param(
 	[string]$tempDirectory = ".",
 	[string]$username = "sdlfly2000",
 	[string]$password = "sdl@1215",
-	[string]$url = "ftp://192.168.31.250/Projects/Configurations",
+	[string]$url = "ftp://192.168.1.25/Projects/Configurations",
 	[string]$configFileName = "app.json"
 )
 
