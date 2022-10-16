4e68278242cde232b5d1359213cedc2ce5521913
Jay Shi <25982097@qq.com>
2022-10-16 20:10:23 +0800

    Initial Log2File

---
+++ Common.Core/LogService/Log2File.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Text;
+
+namespace Common.Core.LogService
+{
+    public class Log2File : ILogger, IDisposable
+    {
+        private readonly StreamWriter _logFileStream;
+
+        public Log2File(IConfiguration configuration)
+        {
+            _logFileStream = File.AppendText(configuration["LogFilePath"]);
+        }
+
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+        {
+            if(_logFileStream == null)
+            {
+                return;
+            }
+
+            var entryBuilder = new StringBuilder(DateTime.UtcNow.ToString());
+            entryBuilder.Append(logLevel.ToString());
+            entryBuilder.Append(eventId.ToString());
+            entryBuilder.Append(nameof(state));
+            entryBuilder.Append(exception);
+            entryBuilder.Append(formatter);
+
+            _logFileStream.Write(entryBuilder.ToString());
+
+        }
+
+        public void Dispose()
+        {
+            if (_logFileStream != null)
+            {
+                _logFileStream.Dispose();
+            }
+        }
+    }
+}
