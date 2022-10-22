3110c8aef07a636cdaada052dc030825ded0cb0a
Jay Shi <25982097@qq.com>
2022-10-22 09:24:40 +0800

    Add Log2File Provider

---
+++ Common.Core/LogService/Log2FileProvider.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Common.Core.LogService
+{
+    public class Log2FileProvider : ILoggerProvider
+    {
+        private readonly IConfiguration _configuration;
+
+        private ILog2File? _logger;
+
+        public Log2FileProvider(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            if (_logger == null)
+            {
+                _logger = new Log2File(_configuration);
+            }
+
+            return _logger;
+        }
+
+        public void Dispose()
+        {
+            if(_logger != null)
+            {
+                _logger.Dispose();
+            }
+        }
+    }
+}
