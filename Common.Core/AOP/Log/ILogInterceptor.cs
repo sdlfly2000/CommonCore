d794210d518dd0f844577eabe9311c2cf6a33836
Jay Shi <25982097@qq.com>
2022-10-23 10:33:53 +0800

    slight refactor

---
+++ Common.Core/AOP/Log/ILogInterceptor.cs
@@ -0,0 +1,6 @@
+﻿namespace Common.Core.AOP.Log
+{
+    internal interface ILogInterceptor : IAOPInterceptor
+    {
+    }
+}
---
+++ Common.Core/AOP/Log/LogInterceptor.cs
@@ -0,0 +1,26 @@
+﻿using Castle.DynamicProxy;
+using Common.Core.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Common.Core.AOP.Log
+{
+    [ServiceLocate(typeof(ILogInterceptor))]
+    public class LogInterceptor : ILogInterceptor
+    {
+        private readonly ILogger<LogInterceptor> _logger;
+
+        public LogInterceptor(ILogger<LogInterceptor> logger)
+        {
+            _logger = logger;
+        }
+
+        public void Intercept(IInvocation invocation)
+        {
+            _logger.LogInformation("Executing: {0}", invocation.MethodInvocationTarget.Name);
+            invocation.Proceed();
+            _logger.LogInformation("Exiting: {0}", invocation.MethodInvocationTarget.Name);
+            _logger.LogInformation("With Result: {0}", JsonConvert.ToString(invocation.ReturnValue));
+        }
+    }
+}
