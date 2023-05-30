f7f6a369e695b7ead8de07bfe9a463f0762f39fd
Jay Shi <25982097@qq.com>
2023-05-31 05:05:08 +0800

    Adding MemoryCacheWraper

---
+++ Common.Core/AOP/Cache/CastleDynamicProxy/IMemoryCacheWraper.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace Common.Core.AOP.Cache.CastleDynamicProxy
+{
+    public interface IMemoryCacheWraper
+    {
+        IMemoryCache MemoryCache { get; }
+    }
+}
