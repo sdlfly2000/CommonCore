046a89699434358669118a79e9554a6a7a56193f
Jay Shi <25982097@qq.com>
2023-08-28 00:42:41 +0800

    Add ActivityLog model

---
+++ Common.Core/LogService/Models/ActivityLog.cs
@@ -0,0 +1,11 @@
+﻿namespace Common.Core.LogService.Models
+{
+    public class ActivityLog : IActivityLog
+    {
+        public string ActivityName { get; set; }
+        public string ActivityVector { get; set; }
+        public string TraceId { get; set; }
+        public int Passed { get; set; }
+        public string Exception { get; set; }
+    }    
+}
